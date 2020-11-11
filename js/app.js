97d057654f6eaa696e7579f7e66fe10d396a08d1
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-11-10 19:12:47 -0500

    add overlay navigation effect, organize files

--- js/app.js
+++ js/app.js
@@ -9,4 +9,19 @@ const hamburger = document.querySelector('.hamburger');
 // Toggle nav menu on mobile screens
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("is-active");
+    if (hamburger.classList.contains("is-active")) {
+        openNav();
+    } else {
+        closeNav();
+    }
 });
+
+// Slide in navigation 
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+// Slide out navigation
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
